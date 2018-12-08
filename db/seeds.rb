fdf6289a59c60d34bc03bf1293329ee4371443d4
Zoe Larsen <zlarsen.zl@gmail.com>
2018-12-08 15:35:25 -0700

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,56 @@
+Product.delete_all
+
+Product.create!(id: 1,
+  title: "Programming Ruby",
+  description:
+  %{<p>
+       Ruby is the fastest growing and most exciting dynamic language out there.
+       If you need to get get working programs deleivered fast, you should add ruby
+       to your toolbox.
+  </p>},
+  image_url: "intro-to-ruby-programming.jpg",
+  price: 49.95)
+
+Product.create!(id: 2,
+  title: "Programming Ruby",
+  description:
+  %{<p>
+       Ruby is the fastest growing and most exciting dynamic language out there.
+       If you need to get get working programs deleivered fast, you should add ruby
+       to your toolbox.
+  </p>},
+  image_url: "intro-to-ruby-programming.jpg",
+  price: 49.95)
+
+Product.create!(id: 3,
+  title: "Programming Ruby",
+  description:
+  %{<p>
+       Ruby is the fastest growing and most exciting dynamic language out there.
+       If you need to get get working programs deleivered fast, you should add ruby
+       to your toolbox.
+  </p>},
+  image_url: "intro-to-ruby-programming.jpg",
+  price: 49.95)
+
+Product.create!(id: 4,
+  title: "Programming Ruby",
+  description:
+  %{<p>
+       Ruby is the fastest growing and most exciting dynamic language out there.
+       If you need to get get working programs deleivered fast, you should add ruby
+       to your toolbox.
+  </p>},
+  image_url: "intro-to-ruby-programming.jpg",
+  price: 49.95)
+
+Product.create!(id: 5,
+  title: "Programming Ruby",
+  description:
+  %{<p>
+       Ruby is the fastest growing and most exciting dynamic language out there.
+       If you need to get get working programs deleivered fast, you should add ruby
+       to your toolbox.
+  </p>},
+  image_url: "intro-to-ruby-programming.jpg",
+  price: 49.95)
