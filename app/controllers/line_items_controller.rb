7e69ef6738d6e83ab7dd242051f2beeb54d65d28
Zoe Larsen <zlarsen.zl@gmail.com>
2018-12-16 19:23:24 -0700

    Cart that hides when empty
    added animation to reveal the cart in the create.js file
    add new helper method to for the cart that is used in the application html
    removed noticed from cart controller destroy action

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,6 @@ class LineItemsController < ApplicationController
   def create
     product    = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,6 @@
 class StoreController < ApplicationController
   include CurrentCart
+
   before_action :set_cart
   def index
     @products = Product.order(:title)
